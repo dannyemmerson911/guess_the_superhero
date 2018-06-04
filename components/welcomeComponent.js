f047d9d35cccbf63da048a243841bf817c652f76
Danny Emmerson <danny.emmerson911@gmail.com>
2018-06-04 19:11:34 -0400

    sweet alert

--- components/welcomeComponent.js
+++ components/welcomeComponent.js
@@ -1,6 +1,28 @@
 (function(){
     const welcomeComponent = {
-        templateUrl: '/partials/welcome.html'
+        templateUrl: '/partials/welcome.html',
+        controller: function() {
+            $ctrl = this;
+            $ctrl.playerName = function() {
+                console.log("click");
+                const {value: name} = swal({
+                    title: 'Enter Your Name',
+                    text: 'What is the player"s name?',
+                    type: 'question',
+                    html: 
+                    '<input id = "name">',
+                    confirmButtonText: 'Let"s play',
+                    inputValidator: function(value) {
+                        return !value && 'You need to write something!'
+                    },
+                    preConfirm: function () {
+                        const name = document.getElementById('name').value;
+                    }
+                })
+                console.log(name);
+            }
+            
+        }
     }
         angular
             .module("myApp")
